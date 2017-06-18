0d7b31b18134176e15e5eb4df5197dd647d7b35f
nick bouldien <nick.bouldien@gmail.com>
2017-06-18 15:23:04 -0700

    work on Home and School routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+
+import Home from './Routes/Home';
+import School from './Routes/Home';
 
 // @flow
 class App extends Component {
+
+
+  // stuff (x: ?number): string {
+  //   if(x) {
+  //     return x;
+  //   }
+  //   return "default string"
+  // }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Switch>
+
+            <Route exact path = "/" component={Home} />
+            <Route exact path = "/school" component={School} />
+
+          </Switch>
+        </Router>
+
       </div>
     );
   }
---
+++ src/Routes/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+const Home = () => (
+      <h1>Hello, World</h1>
+)
+
+export default Home;
